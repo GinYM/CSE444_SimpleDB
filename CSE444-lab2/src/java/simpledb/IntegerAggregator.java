acda5a5c35bd6586a12b269cfd53f0cb1352dd2b
GinYM <jinyiming1996@gmail.com>
2018-09-15 15:19:28 -0700

    Integer Aggregate

--- CSE444-lab2/src/java/simpledb/IntegerAggregator.java
+++ CSE444-lab2/src/java/simpledb/IntegerAggregator.java
@@ -1,5 +1,7 @@
 package simpledb;
 
+import java.util.*;
+
 /**
  * Knows how to compute some aggregate over a set of IntFields.
  */
@@ -22,8 +24,20 @@ public class IntegerAggregator implements Aggregator {
      *            the aggregation operator
      */
 
+    private int gbfield;
+    private Type gbfieldtype;
+    private int afield;
+    private Op what;
+
+    private Map<Field, List<Tuple>> map;
+
     public IntegerAggregator(int gbfield, Type gbfieldtype, int afield, Op what) {
         // some code goes here
+        this.gbfield = gbfield;
+        this.gbfieldtype = gbfieldtype;
+        this.what = what;
+        this.afield = afield;
+        map = new HashMap<>();
     }
 
     /**
@@ -35,6 +49,12 @@ public class IntegerAggregator implements Aggregator {
      */
     public void mergeTupleIntoGroup(Tuple tup) {
         // some code goes here
+        //System.out.println("In Merge: "+tup);
+        Field fd = tup.getField(gbfield);
+        if(map.containsKey(fd) == false){
+            map.put(fd, new ArrayList<>());
+        }
+        map.get(fd).add(tup);
     }
 
     /**
@@ -45,10 +65,91 @@ public class IntegerAggregator implements Aggregator {
      *         aggregateVal is determined by the type of aggregate specified in
      *         the constructor.
      */
+    class MyIter implements DbIterator{
+
+        private Iterator<Map.Entry<Field, List<Tuple>>> iter;
+        private boolean isOpen;
+        private TupleDesc tdes;
+        public MyIter(){
+
+            isOpen = false;
+        }
+        public void open()
+                throws DbException, TransactionAbortedException{
+            iter = map.entrySet().iterator();
+            isOpen = true;
+        }
+        public boolean hasNext() throws DbException, TransactionAbortedException{
+            if(isOpen == false) throw new IllegalStateException();
+            return iter.hasNext();
+        }
+
+        public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException{
+            if(hasNext() == false) throw new NoSuchElementException();
+            if(isOpen == false) throw new IllegalStateException();
+
+            Map.Entry<Field, List<Tuple>> pair = iter.next();
+            int result = 0;
+            if(what == Op.MIN){
+                result = Integer.MAX_VALUE;
+            }else if(what == Op.MAX){
+                result = Integer.MIN_VALUE;
+            }else{
+                result = 0;
+            }
+
+            for(Tuple tp : pair.getValue()){
+                //System.out.println("In for loop: "+tp+" "+gbfield+" "+afield);
+                switch(what){
+                    case MIN:
+                        result = Math.min(result, ((IntField)tp.getField(afield)).getValue());
+                        break;
+                    case MAX:
+                        result = Math.max(result, ((IntField)tp.getField(afield)).getValue());
+                        break;
+                    default:
+                        result += ((IntField)tp.getField(afield)).getValue();
+                        //System.out.println(result);
+                        break;
+                }
+            }
+
+            TupleDesc td = null;
+            Type typeArr [] = new Type[2];
+            String fieldAr[] = new String[2];
+            typeArr[0] = gbfieldtype;
+            typeArr[1] = Type.INT_TYPE;
+            fieldAr[0] = "GroupBy";
+            fieldAr[1] = "Aggregate";
+            if(what == Op.AVG) result /= pair.getValue().size();
+            else if(what == Op.COUNT) result = pair.getValue().size();
+
+            td = new TupleDesc(typeArr, fieldAr);
+            Tuple ret = new Tuple(td);
+            ret.setField(0, pair.getKey());
+            ret.setField(1, new IntField(result));
+
+            return ret;
+        }
+
+        public void rewind() throws DbException, TransactionAbortedException{
+            if(isOpen == false) throw new IllegalStateException();
+            open();
+        }
+
+        public TupleDesc getTupleDesc(){
+            return tdes;
+        }
+
+        public void close(){
+            isOpen = false;
+        }
+
+    }
+
     public DbIterator iterator() {
         // some code goes here
-        throw new
-        UnsupportedOperationException("please implement me for lab2");
+        return new MyIter();
     }
 
 }
